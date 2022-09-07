53faa9c072f11d19327f1a5cfb27a75d18fd08bc
sushmitha <sushmitha2973@gmail.com>
2022-09-07 22:11:00 +0530

    Handling user authentication

---
+++ Practice_project3/src/main/java/com/UserAuthentication/App.java
@@ -0,0 +1,9 @@
+package com.UserAuthentication;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Practice_project3/src/main/java/com/UserAuthentication/UserAuthentication.java
@@ -0,0 +1,17 @@
+package com.UserAuthentication;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "Admin";
+	}
+	public String getEmail() {
+		return "admin@gmail.com";
+	}
+	public int getPhnNum() {
+		return 1010;
+	}
+	public String getPassword() {
+		return "Abc123";
+	}
+
+}
